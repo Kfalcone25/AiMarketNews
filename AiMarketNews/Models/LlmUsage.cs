3ca13e079f3015a709231626c44b60c39b8d2f87
Kflacone <kfalcone@dlr360.com>
2026-04-08 10:39:56 -0600

    Added the LLM service with API documentation as we make calls to the api for token usage

---
+++ AiMarketNews/Models/LlmUsage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiMarketNews.Models
+{
+	public class LlmUsage
+	{
+		//Tracks API Usage for LLM calls, including input and output token counts, to monitor costs and optimize prompts.
+		public int InputTokens { get; set; }
+		public int OutputTokens { get; set; }
+		public int TotalTokens => InputTokens + OutputTokens;
+	}
+}
---
+++ AiMarketNews/Services/NewsService/INewsService.cs
@@ -0,0 +1,12 @@
+﻿using AiMarketNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiMarketNews.Services.NewsService
+{
+	public interface INewsService
+	{
+		List<Article> GetTopHeadlines();
+	}
+}
